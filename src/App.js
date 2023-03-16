48ae4da8fe9f4cd6c872891c0867dfd01ad301c6
kjh-busan <picsmash7@gmail.com>
2023-03-17 01:15:35 +0900

    feat: style css basic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 const App = () => {
-  // const para = document.createElement('p');
-  // para.textContent = 'This is also visible';
-  // document.getElementById('root').append(para);
   return (
     <div>
-      <h2>Hello World</h2>
-      <p>this is also visible jsx</p>
+      <h2>Expense Calculator</h2>
+      <ExpenseItem />
     </div>
-  )
+  );
 };
 
 export default App;
