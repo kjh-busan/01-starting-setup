1ff407b1256aeb6ad6da9b557e92d68bd1f55d1e
kjh-busan <picsmash7@gmail.com>
2023-03-17 00:34:21 +0900

    feat: input vanillaJS used

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 const App = () => {
+  const para = document.createElement('p');
+  para.textContent = 'This is also visible';
+  document.getElementById('root').append(para);
   return (
     <h2>Hello World</h2>
   )
