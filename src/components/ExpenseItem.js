713a671eed17f19cf14c81883cbf62eb6cf5f960
kjh-busan <picsmash7@gmail.com>
2023-03-17 01:36:41 +0900

    feat: props1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2023, 2, 16);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+const ExpenseItem = ({ expense }) => {
+  // const expenseDate = new Date(2023, 2, 16);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 294.67;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{expense.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{expense.title}</h2>
+        <div className="expense-item__price">${expense.amount}</div>
       </div>
     </div>
   );
