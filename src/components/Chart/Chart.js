61dc051e715e661d79b6ac7ad2f164b03f36115e
kjh-busan <picsmash7@gmail.com>
2023-03-19 02:04:37 +0900

    fix: chart component error WIP

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,7 +1,22 @@
 import React from "react";
+import ChartBar from "./ChartBar";
+import "./Chart.css";
 
-const Chart = () => {
-  return <div></div>;
+const Chart = (props) => {
+  const dataPoinValue = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPoinValue);
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.idf}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Chart;
